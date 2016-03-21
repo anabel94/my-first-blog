b40ba49f7692d2e6d9be9f1de3eecc4bf8e15834
anabel94 <achafer@campusaula.com>
2016-03-21 17:36:03 +0100

    Modificando vista

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,7 @@ def post_new(request):
          if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
       else:
